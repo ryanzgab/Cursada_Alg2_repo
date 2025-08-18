83805fd62717b3bc3f8e18f1fdd30a33eefde2f1
Medina Gabriel <medina1931@hotmail.com>
2025-08-18 18:26:11 -0300

    new file:   .vscode/settings.json 	renamed:    Estructura dinamica/TP1/Punteros/Punteros_AsignacionDinamica.c -> Estructura dinamica/TP1/Punteros-Plantillas/Punteros_AsignacionDinamica.c 	renamed:    Estructura dinamica/TP1/Punteros/Punteros_AsignacionEstatica.c -> Estructura dinamica/TP1/Punteros-Plantillas/Punteros_AsignacionEstatica.c 	renamed:    Estructura dinamica/TP1/Punteros/output/Punteros_AsignacionDinamica.exe -> Estructura dinamica/TP1/Punteros-Plantillas/output/Punteros_AsignacionDinamica.exe 	renamed:    Estructura dinamica/TP1/Punteros/output/Punteros_AsignacionEstatica.exe -> Estructura dinamica/TP1/Punteros-Plantillas/output/Punteros_AsignacionEstatica.exe 	renamed:    Estructura dinamica/TP1/Punteros/output/TP1-1 punteros.exe -> Estructura dinamica/TP1/Punteros-Plantillas/output/TP1-1 punteros.exe 	renamed:    Estructura dinamica/TP1/Punteros/puntero_bool.c -> Estructura dinamica/TP1/Punteros-Plantillas/puntero_bool.c 	renamed:    Estructura dinamica/TP1/Punteros/puntero_entero.c -> Estructura dinamica/TP1/Punteros-Plantillas/puntero_entero.c 	renamed:    Estructura dinamica/TP1/punto-1.c -> Estructura dinamica/TP1/Punto 1/punto-1a.c 	new file:   Estructura dinamica/TP1/Punto 1/punto-1b.c 	renamed:    Estructura dinamica/TP1/punto-2.c -> Estructura dinamica/TP1/Punto 2/punto-2.c 	new file:   Estructura dinamica/TP1/output/punto-1.exe

--- Estructura dinamica/TP1/Punto 1/punto-1a.c
+++ Estructura dinamica/TP1/Punto 1/punto-1a.c
@@ -1,5 +1,5 @@
-/*1. Utilizar la tabla proporcionada para representar el estado de la memoria. Completar los espacios en blanco
-con la dirección de memoria o el valor correspondiente según la consigna.*/
+/*a) Una variable puntero (apuntInt) de tipo int, ubicada en la dirección 0050, se inicializa para apuntar a la
+dirección 0054. La dirección 0054 contiene el dato 10.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,8 +18,6 @@ int main()
     return 0;
 }
 
-/*a) Una variable puntero (apuntInt) de tipo int, ubicada en la dirección 0050, se inicializa para apuntar a la
-dirección 0054. La dirección 0054 contiene el dato 10.*/
 void tipoInt()
 {
     int intCosa = 10;
@@ -39,20 +37,7 @@ void tipoInt()
     printf("\n---------------------------------\n");    
 }
 
-/*b) A un puntero de tipo char (pChar) cuya dirección es 00A0 se le asigna memoria dinámica en la dirección
-00AB y se le asigna el valor 'A'.*/
 
-void tipoChar()
-{
-    char Ccosa = 'A';
-
-    pChar = &Ccosa;
-
-    printf("\nEl contenido de mondongo es: %d\n", * pChar);
-    printf("\nLa direccion de mondongo es: %p\n",   pChar);
-    printf("\nla direccion del apuntador es: %p\n", &pChar);
-    printf("\n---------------------------------\n");
-}
 
 /*c) A un puntero de tipo float (pFloat) que se encuentra almacenado en la posición 001A se le asigna
 memoria dinámica en la dirección 00B0 y se le asigna el valor 3.14.*/
@@ -63,7 +48,7 @@ void tipoFlooat()
 
     pFloat = &Fcosa;
 
-    printf("\nEl contenido de mondongo es: %d\n", * pFloat);
+    printf("\nEl contenido de mondongo es: %f\n", * pFloat);
     printf("\nLa direccion de mondongo es: %p\n",   pFloat);
     printf("\nla direccion del apuntador es: %p\n", &pFloat);
     printf("\n---------------------------------\n");
