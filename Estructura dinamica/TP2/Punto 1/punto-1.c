a65e75f354ced0e35a8d25af0ca23f6f732cc0a8
Medina Gabriel <medina1931@hotmail.com>
2025-08-20 21:39:21 -0300

    new file:   Estructura dinamica/TP2/Punto 1/punto-1.c

---
+++ Estructura dinamica/TP2/Punto 1/punto-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXCARD 50
+
+
+typedef char tstring[MAXCARD];
+
+typedef struct 
+{
+    int idJuego;
+    tstring titulo;
+    int anoLanzamiento;
+    float precio;
+}tDatosVideoJuegos;
+
+typedef struct Nodo 
+{
+
+}
