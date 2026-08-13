624fb88418e1183c897b4d282d0cb0dacbbb6a6f
Medina Gabriel <medina1931@hotmail.com>
2026-08-13 15:36:59 -0300

    new file:   AEDII/2026/TP1/Punto-1(a,b,c).png 	new file:   AEDII/2026/TP1/Punto-1(d).c 	modified:   codigos Varios/prueba.c

---
+++ AEDII/2026/TP1/Punto-1(d).c
@@ -0,0 +1,23 @@
+/*
+ ███  █████ ████  ███ ███            ████  █   █ █   █ █████ █████ ████   ███   ████ 
+█   █ █     █   █  █   █             █   █ █   █ ██  █   █   █     █   █ █   █ █     
+█████ ████  █   █  █   █     ████    ████  █   █ █ █ █   █   ████  ████  █   █  ███  
+█   █ █     █   █  █   █             █     █   █ █  ██   █   █     █  █  █   █     █ 
+█   █ █████ ████  ███ ███            █      ███  █   █   █   █████ █   █  ███  ████  
+*/
+
+/*
+- NOM. PROGRAMA: Punto-1-(d).c
+- NOM. PROGRAMADOR: Medina Juan Gabriel
+- DESCRIPCION: Escribir un programa en C que implemente las situaciones anteriores, mostrando en la consola las direcciones de memoria y los valores del dato antes y después 
+de la modificación de cada una de las variables que intervienen. Utilizar asignación estática y dinámica (malloc). 
+- REVISION: [NINGUNA]
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
