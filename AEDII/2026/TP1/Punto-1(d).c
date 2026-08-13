a73859becd61a3491c2e60987c0a051c1b56f2d9
Medina Gabriel <medina1931@hotmail.com>
2026-08-13 16:27:20 -0300

    modified:   AEDII/2026/TP1/Punto-1(d).c 	new file:   AEDII/2026/TP1/output/Punto-1(d).exe 	new file:   Estructura dinamica/TP1/Punto 2/output/punto-2.exe

--- AEDII/2026/TP1/Punto-1(d).c
+++ AEDII/2026/TP1/Punto-1(d).c
@@ -8,7 +8,7 @@
 
 /*
 - NOM. PROGRAMA: Punto-1-(d).c
-- NOM. PROGRAMADOR: Medina Juan Gabriel
+- NOM. ALUMNO: Medina Juan Gabriel
 - DESCRIPCION: Escribir un programa en C que implemente las situaciones anteriores, mostrando en la consola las direcciones de memoria y los valores del dato antes y después 
 de la modificación de cada una de las variables que intervienen. Utilizar asignación estática y dinámica (malloc). 
 - REVISION: [NINGUNA]
@@ -16,8 +16,64 @@ de la modificación de cada una de las variables que intervienen. Utilizar asign
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int data;
+char caracter;
+float flotante;
+
+void casoA();
+void casoB();
+void casoC();
 
 
+int main()
+{
+    casoB();
+    return 0;
+}
+
+void casoA()
+{
+    int* ApData;
+    ApData = (int*)malloc(sizeof(int)); /*Asignacion de memoria*/
+
+    data = 10;
+    ApData = &data;
 
 
+    printf("Direccion de memoria de puntero es:%p \n", &ApData);
+    printf("Contenido de mi variable puntero: %p \n", ApData);
+    printf("Direccion de la Variable Data: %p\n", &data);
+    printf("El valor entero es: %d \n", *ApData);
+}
 
+void casoB()
+{
+    char* ApData;
+    ApData = (char*)malloc(sizeof(char)); /*Asignacion de memoria*/
+
+    caracter = 'A';
+    ApData = &caracter;
+
+
+    printf("Direccion de memoria de puntero es:%p \n", &ApData);
+    printf("Contenido de mi variable puntero: %p \n", ApData);
+    printf("Direccion de la Variable Data: %p\n", &caracter);
+    printf("El valor entero es: %c \n", *ApData);
+}
+
+void casoC()
+{
+    float* ApData;
+    ApData = (float*)malloc(sizeof(float)); /*Asignacion de memoria*/
+
+    flotante = 'A';
+    ApData = &;
+
+
+    printf("Direccion de memoria de puntero es:%p \n", &ApData);
+    printf("Contenido de mi variable puntero: %p \n", ApData);
+    printf("Direccion de la Variable Data: %p\n", &caracter);
+    printf("El valor entero es: %c \n", *ApData);
+}
