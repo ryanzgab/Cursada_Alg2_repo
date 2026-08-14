74df6682ebda80d49cdd01c8fb9ccde56c5ff124
Medina Gabriel <medina1931@hotmail.com>
2026-08-13 23:22:38 -0300

    modified:   AEDII/TP1/Punto-4.c

--- AEDII/TP1/Punto-4.c
+++ AEDII/TP1/Punto-4.c
@@ -19,19 +19,32 @@
 typedef float Real;
 typedef Real* ApReal;
 
+/*
+    a) - ¿Qué tipo de dato es ApTemperatura? ¿Y qué tipo de dato contiene?
+    R: es de tipo "Real" y contiene un dato de tipo Float.
+
+    b) - ¿Qué contiene ApTemperatura justo después de declararse?
+    R: Contiene solamente su propia direccion de memoria.
+*/
 int main()
 {
-    /*
-    a#- 
-
-    */
     Real temperatura;
     ApReal ApTemperatura;
 
     ApTemperatura=(float*)malloc(sizeof(float));
     *ApTemperatura= 5.5;
     temperatura= 3.3;
-
-
     return 0;
+    /*
+    4) :
+            I. ¿Qué valor contiene ApTemperatura? 
+                R: Su propia direccion de memoria.
+            II. ¿Qué valor contiene *ApTemperatura?
+                R: Ahora ademas de su direccion de memoria contiene el dato de tipo float 5.5
+            III. ¿Qué valor contiene temperatura? 
+                R: Contiene unicamente el dato de tipo float 3.3
+            IV. ¿punteroTemperatura apunta al mismo lugar que &temperatura?
+                R: No porque ambos tienen direcciones de memoria diferentes.
+*/
+
 }
