15e79120c5b6b8729909e26933799a307ac13253
Medina Gabriel <medina1931@hotmail.com>
2026-08-13 22:13:56 -0300

    new file:   AEDII/TP1/Punto-3.c

---
+++ AEDII/TP1/Punto-3.c
@@ -0,0 +1,16 @@
+/*
+ ███  █████ ████  ███ ███            ████  █   █ █   █ █████ █████ ████   ███   ████ 
+█   █ █     █   █  █   █             █   █ █   █ ██  █   █   █     █   █ █   █ █     
+█████ ████  █   █  █   █     ████    ████  █   █ █ █ █   █   ████  ████  █   █  ███  
+█   █ █     █   █  █   █             █     █   █ █  ██   █   █     █  █  █   █     █ 
+█   █ █████ ████  ███ ███            █      ███  █   █   █   █████ █   █  ███  ████  
+*/
+
+/*
+- NOM. PROGRAMA: Punto-3.c
+- NOM. ALUMNO: Medina Juan Gabriel
+- DESCRIPCION: Programa con una similitud con el punto anterior pero en cambio, se maneja con una estructura de int 
+y char. Con el agregado de un puntero adicional que apunte a la estructura y la modifica en cada campo.
+- REVISION: [NINGUNA]
+*/
+
